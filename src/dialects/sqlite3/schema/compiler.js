65e5755bd71cf95412c099be0868ebf7bd51ea38
amine <benahmed.education@gmail.com>
2022-08-29 17:53:53 +0100

    fix: hasTable return allways false

--- src/dialects/sqlite3/schema/compiler.js
+++ src/dialects/sqlite3/schema/compiler.js
@@ -1,10 +1,9 @@
-
 // SQLite3: Column Builder & Compiler
 // -------
-import inherits from 'inherits';
-import SchemaCompiler from '../../../schema/compiler';
+import inherits from "inherits";
+import SchemaCompiler from "../../../schema/compiler";
 
-import { some } from 'lodash'
+import { some } from "lodash";
 
 // Schema Compiler
 // -------
@@ -15,26 +14,30 @@ function SchemaCompiler_SQLite3() {
 inherits(SchemaCompiler_SQLite3, SchemaCompiler);
 
 // Compile the query to determine if a table exists.
-SchemaCompiler_SQLite3.prototype.hasTable = function(tableName) {
+SchemaCompiler_SQLite3.prototype.hasTable = function (tableName) {
   const sql =
     `select * from sqlite_master ` +
     `where type = 'table' and name = ${this.formatter.parameter(tableName)}`;
-  this.pushQuery({ sql, output: resp => resp.length > 0 });
+  this.pushQuery({ sql, output: (resp) => resp.rows.length > 0 });
 };
 
 // Compile the query to determine if a column exists.
-SchemaCompiler_SQLite3.prototype.hasColumn = function(tableName, column) {
+SchemaCompiler_SQLite3.prototype.hasColumn = function (tableName, column) {
   this.pushQuery({
     sql: `PRAGMA table_info(${this.formatter.wrap(tableName)})`,
     output(resp) {
-      return some(resp, {name: column});
-    }
+      return some(resp, { name: column });
+    },
   });
 };
 
 // Compile a rename table command.
-SchemaCompiler_SQLite3.prototype.renameTable = function(from, to) {
-  this.pushQuery(`alter table ${this.formatter.wrap(from)} rename to ${this.formatter.wrap(to)}`);
+SchemaCompiler_SQLite3.prototype.renameTable = function (from, to) {
+  this.pushQuery(
+    `alter table ${this.formatter.wrap(from)} rename to ${this.formatter.wrap(
+      to
+    )}`
+  );
 };
 
 export default SchemaCompiler_SQLite3;
